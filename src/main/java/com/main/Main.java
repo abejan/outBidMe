511a621585fef6f0a6fe97e38eb8cd156b8a4e97
Eugen M <madeaneugen@gmail.com>
2014-05-04 17:01:23 +0300

    Eugen's first commit

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -3,6 +3,6 @@ package com.main;
 public class Main {
 
     public static void main(String args[]){
-        System.out.println("Hello OutBidMe!!!");
+        System.out.println("Hello OutBidMe!!!...");
     }
 }
